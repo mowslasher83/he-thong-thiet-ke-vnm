d41c7f9a0b9ccbc57f2668430d45038f79da565b
mowslasher83 <radeswaled@hotmail.com>
2025-09-24 09:09:22 -0500

    Fix button click event to call onClick function

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 export const Button = ({ label, onClick }) => {
-  return `<button>${label}</button>`;
+  return `<button onclick="${onClick}">${label}</button>`;
 };
 
 export const setButtonClick = (element, onClick) => {
