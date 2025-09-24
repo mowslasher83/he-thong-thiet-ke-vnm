935887afb08ad739569b5814f673a914c0c3595d
mowslasher83 <radeswaled@hotmail.com>
2025-09-24 09:14:11 -0500

    Fix Button component to handle click event properly

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
 export const Button = ({ label, onClick }) => {
-  return `<button>${label}</button>`;
-};
-
-export const setButtonClick = (element, onClick) => {
-  element.addEventListener('click', onClick);
-};
+  return `<button onclick=\
